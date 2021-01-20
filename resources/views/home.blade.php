78e3ccfe8d82b89ce236c52ea4f1c3aa91ffd6f6
giorgi <gionachye15@gmail.com>
2021-01-21 02:49:38 +0400

    comented carousel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,49 +1,10 @@
 
 @extends('details/master')
 @section('content')
-{{-- <div class="slider">
-    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel" data-interval="false">
-        <ol class="carousel-indicators">
-            <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"></li>
-            <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"></li>
-            <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="https://raw.githubusercontent.com/isocpp/logos/master/cpp_logo.png" class="d-block h-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>First slide label</h5>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="https://tetraxbalance.com/wp-content/uploads/2016/03/js-logo.png" class="d-block h-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Second slide label</h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="https://cdn.freebiesupply.com/logos/large/2x/php-1-logo-png-transparent.png" class="d-block h-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Third slide label</h5>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                </div>
-            </div>
-        </div>
-        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </a>
-    </div>
-</div> --}}
+
 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
     <div class="carousel-inner">
-        @foreach ($posts as $item)
+        {{-- @foreach ($posts as $item)
        
         <div class="carousel-item {{$loop->first ?'active':''}}">
             <img src="{{Storage::url($item->img)}}" class="d-block h-100" alt="...">
@@ -52,7 +13,14 @@
                 <p>{{$item->description}}</p>
               </div>
         </div>
-        @endforeach
+        @endforeach --}}
+        <div class="carousel-item active">
+            <img src="https://www.allianceplast.com/wp-content/uploads/2017/11/no-image.png" class="d-block h-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>}</h5>
+                <p></p>
+              </div>
+        </div>
      
 
     </div>
